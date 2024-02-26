9779f5da33c11e2360e81bc086c6938f5295e91f
thoser666 <thoser666@web.de>
2024-02-26 14:12:51 +0100

    Initial commit

---
+++ src/main/java/biz/brumm/thenursejava/TheNurseJavaApplication.java
@@ -0,0 +1,13 @@
+package biz.brumm.thenursejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNurseJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheNurseJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/biz/brumm/thenursejava/TheNurseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.brumm.thenursejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNurseJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
