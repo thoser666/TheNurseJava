cdba174cf2dfc0fa5be8e8356ebdfa3bc07cd104
thoser666 <thoser666@web.de>
2024-03-06 12:40:14 +0100

    Test added

---
+++ src/test/java/biz/brumm/thenursejava/dto/AuthResponseDTOTest.java
@@ -0,0 +1,49 @@
+package biz.brumm.thenursejava.dto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AuthResponseDTOTest {
+    AuthResponseDTO authResponseDTO = new AuthResponseDTO();
+
+    @Test
+    void testSetAuthenticationToken() {
+        authResponseDTO.setAuthenticationToken("authenticationToken");
+    }
+
+    @Test
+    void testSetUsername() {
+        authResponseDTO.setUsername("username");
+    }
+
+    @Test
+    void testSetIsAdmin() {
+        authResponseDTO.setIsAdmin("isAdmin");
+    }
+
+    @Test
+    void testEquals() {
+        boolean result = authResponseDTO.equals("o");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    void testCanEqual() {
+        boolean result = authResponseDTO.canEqual("other");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    void testHashCode() {
+        int result = authResponseDTO.hashCode();
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    void testToString() {
+        String result = authResponseDTO.toString();
+        Assertions.assertEquals("replaceMeWithExpectedResult", result);
+    }
+}
+
+//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
